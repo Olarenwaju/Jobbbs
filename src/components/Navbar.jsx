03673d6abee0bb67fcd68f3cc603f807ed63a706
DevLaren <larenthedev@gmail.com>
2024-10-03 08:21:02 +0100

    vercel deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 //import React from 'react'
 import { useEffect, useState } from "react"
 import {Link} from "react-router-dom"
-
 import jobslogo from "../assets/img/jobbbs.png"
 
 const Navbar = () => {
